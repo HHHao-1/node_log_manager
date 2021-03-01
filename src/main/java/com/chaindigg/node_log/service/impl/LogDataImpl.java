625ec1b86ca6efe702437aac081fccb563512018
chenghao <454947233@qq.com>
2021-03-01 19:12:25 +0800

    每行数据异常处理、重构代码

--- src/main/java/com/chaindigg/node_log/service/impl/LogDataImpl.java
+++ src/main/java/com/chaindigg/node_log/service/impl/LogDataImpl.java
@@ -20,32 +20,32 @@ import java.util.stream.Collectors;
 @Slf4j
 @Service
 public class LogDataImpl implements ILogDataService {
-
+  
   @Value("${var.log.path}")
   private String logPath;
-
+  
   @Value("${var.log.loop.batch}")
   private Integer batch;
-
+  
   @Value("${var.log.string.rowKeySplice}")
   private String rowKeySplice;
-
+  
   // region data
   @Value("${var.log.string.txid.start}")
   private String txidStart;
   @Value("${var.log.string.txid.end}")
   private String txidEnd;
-
+  
   @Value("${var.log.string.ip.start}")
   private String ipStart;
   @Value("${var.log.string.ip.end}")
   private String ipEnd;
-
+  
   @Value("${var.log.string.time.start}")
   private String timeStart;
   @Value("${var.log.string.time.end}")
   private String timeEnd;
-
+  
   @Value("${var.log.string.txid.addEndIndex}")
   private Integer txidAddEndIndex;
   @Value("${var.log.string.ip.addEndIndex}")
@@ -53,10 +53,13 @@ public class LogDataImpl implements ILogDataService {
   @Value("${var.log.string.time.addEndIndex}")
   private Integer timeAddEndIndex;
   // endregion
-
+  
   @Resource
   private IHbaseService hbaseService;
-
+  
+  private List<String> lines = new ArrayList<>();
+  private List<LogEntity> logEntities = new ArrayList<>();
+  
   @Override
   public void saveList(String date) throws IOException {
     if (date == null || date.equals("")) {
@@ -72,55 +75,31 @@ public class LogDataImpl implements ILogDataService {
       }
     }
   }
-
+  
   private void save(File file) throws IOException {
     Files.walkFileTree(
         Paths.get(file.getPath()),
         new SimpleFileVisitor<Path>() {
           @Override
-          public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
-              throws IOException {
-            List<LogEntity> logEntities = new ArrayList<>();
+          public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
             if (file.toString().contains(".log")) {
               String nodeName = file.getParent().getFileName().toString();
               log.info("node: {}, file : {}", nodeName, file.getFileName().toString());
               try (Scanner sc = new Scanner(file)) {
                 int sum = 0;
+                int flag = 0;
                 while (sc.hasNextLine()) {
+                  flag++;
                   sum++;
                   log.info("loop {} start...,batch: {}", sum, batch);
-                  List<String> lines = new ArrayList<>();
-                  for (int i = 0; i < batch; i++) {
-                    if (sc.hasNextLine()) {
-                      lines.add(sc.nextLine());
-                    }
+                  lines.add(sc.nextLine());
+                  if (flag > batch || !sc.hasNextLine()) {
+                    buildDataToHbase(nodeName);
+                    matchSave(sum);
+                    flag = 0;
+                    lines = new ArrayList<>();
+                    logEntities = new ArrayList<>();
                   }
-                  lines.stream().collect(
-                      Collectors.toMap(
-                          k -> {
-                            String ip = k.substring(k.indexOf(ipStart) + ipStart.length(),
-                                k.indexOf(ipEnd) + ipAddEndIndex);
-                            String txid = k.substring(k.indexOf(txidStart) + txidStart.length());
-                            return txid + rowKeySplice + ip + rowKeySplice + nodeName;
-                          },
-                          v -> v.substring(v.indexOf(timeStart) + timeStart.length(),
-                              v.indexOf(timeEnd) + timeAddEndIndex),
-                          (oldVal, currVal) -> oldVal))
-                      .forEach((k, v) -> logEntities.add(new LogEntity(k, v)));
-                  List<String> rowKey =
-                      logEntities.parallelStream().map(LogEntity::getKey).collect(Collectors.toList());
-                  log.info("before connect hbase to batchGet,total = {}",rowKey.size());
-                  Map<String, LogEntity> stringLogEntityMap = hbaseService.batchGet(rowKey);
-                  log.info("after connect hbase to batchGet");
-                  Set<String> keys = stringLogEntityMap.keySet();
-                  log.info("before removeAll");
-                  List<LogEntity> logEntityList =
-                      logEntities.parallelStream().filter(s -> !keys.contains(s.getKey())).collect(Collectors.toList());
-//                  logEntities.removeAll(values);
-                  log.info("before save to hbase");
-//                  hbaseService.saveList(logEntities);
-                  hbaseService.saveList(logEntityList);
-                  log.info("loop {} end", sum);
                 }
               } catch (Exception e) {
                 e.printStackTrace();
@@ -128,19 +107,66 @@ public class LogDataImpl implements ILogDataService {
             }
             return FileVisitResult.CONTINUE;
           }
+          
+          private void matchSave(int sum) throws Exception {
+            List<String> rowKey =
+                logEntities.parallelStream().map(LogEntity::getKey).collect(Collectors.toList());
+            log.info("before connect hbase to batchGet,total = {}", rowKey.size());
+            Map<String, LogEntity> stringLogEntityMap = hbaseService.batchGet(rowKey);
+            log.info("after connect hbase to batchGet");
+            Set<String> keys = stringLogEntityMap.keySet();
+            log.info("before removeAll");
+            List<LogEntity> logEntityList =
+                logEntities.parallelStream().filter(s -> !keys.contains(s.getKey())).collect(Collectors.toList());
+            log.info("before save to hbase");
+            hbaseService.saveList(logEntityList);
+            log.info("loop {} end", sum);
+          }
+          
+          private void buildDataToHbase(String nodeName) {
+            lines.stream().collect(
+                Collectors.toMap(
+                    k -> {
+                      try {
+                        String ip = k.substring(k.indexOf(ipStart) + ipStart.length(),
+                            k.indexOf(ipEnd) + ipAddEndIndex);
+                        String txid = k.substring(k.indexOf(txidStart) + txidStart.length());
+                        return txid + rowKeySplice + ip + rowKeySplice + nodeName;
+                      } catch (Exception e) {
+                        e.printStackTrace();
+                        log.info("hbase---rowKey字符串构造异常");
+                        return "rowKeyError";
+                      }
+                    },
+                    v -> {
+                      try {
+                        return v.substring(v.indexOf(timeStart) + timeStart.length(),
+                            v.indexOf(timeEnd) + timeAddEndIndex);
+                      } catch (Exception e) {
+                        e.printStackTrace();
+                        log.info("hbase--value字符串构造异常");
+                        return "valueError";
+                      }
+                    },
+                    (oldVal, currVal) -> oldVal))
+                .forEach((k, v) -> {
+                  if (!Objects.equals(k, "rowKeyError") || !Objects.equals(v, "valueError"))
+                    logEntities.add(new LogEntity(k, v));
+                });
+          }
         });
   }
-
+  
   @Override
   public LogEntity getOne(String rowKey) throws Exception {
     return hbaseService.get(rowKey);
   }
-
+  
   @Override
   public List<LogEntity> batchGet(List<String> rowKeys) throws Exception {
     return hbaseService.fuzzyScan(rowKeys);
   }
-
+  
   @Override
   //  @Scheduled(cron = "0 0 5 * * ?")
   @Scheduled(cron = "${var.cron.date}")
